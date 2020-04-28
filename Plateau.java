b08807dd65930ddc6649a7920f802b38c1e0f3d4
karinamartinez22 <62945646+karinamartinez22@users.noreply.github.com>
2020-04-28 18:38:06 +0200

    mode 2 3 4 6

--- Plateau.java
+++ Plateau.java
@@ -4,10 +4,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Paint;
-import java.util.LinkedList;
-
+import java.util.*;
 import javax.swing.JPanel;
-
 
 public class Plateau extends JPanel {
 
@@ -16,7 +14,9 @@ public class Plateau extends JPanel {
 	Couleur_du_pion couleur;
 	private Cases_du_plateau caseActive;
 
-	private boolean tourNoir;
+	private tour tourjoueur=tour.tourVERT;;
+	private int nbjoueurs;
+
 	private boolean case_valide;
 	private boolean dejaSaute;
 	private LinkedList<Coordonees> mouvementsPossibles = new LinkedList<>();
@@ -33,51 +33,105 @@ public class Plateau extends JPanel {
 	
 
 	public Plateau(int taille){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir le nombre de joueurs (2 , 3 , 4 ou 6) :");
+		String a = sc.nextLine();
+		nbjoueurs=Integer.parseInt(a);
 		setLayout(new GridLayout(taille,taille));
 		ajouterCoordonees();
 		for(int i=0; i<taille; i++){	// dans cette boucle, on va colorier les cases du plateau
 			for(int j=0; j<taille; j++){
-				
-				if ((j%2!=0 && i%2!=0) || (i%2==0 && j%2==0) || (i>=13) || (j<=6 && i<=2) || (j>=12 && i<=2) || (i<=12 && i>=10 && j<=6) || (j>=12 && i<=12 && i>=10) || ( j==0 && i>=4 && i<=8) || ( j==18 && i>=4 && i<=8) || (j==1 && i<=7 && i>=5) || (j==17 && i<=7 && i>=5) || (j==7 && i==12) || (j==7 && i==0) || (j==11 && i==0) || (j==11 && i==12)){
-					ajouterCase(Couleur_du_case.GRIS);
-				}
-					else{
-						
+				if(nbjoueurs==2){	
+					if ((j%2!=0 && i%2!=0) || (i%2==0 && j%2==0) || (i>=13) || (j<=6 && i<=2) || (j>=12 && i<=2) || (i<=12 && i>=10 && j<=6) || (j>=12 && i<=12 && i>=10) || ( j==0 && i>=4 && i<=8) || ( j==18 && i>=4 && i<=8) || (j==1 && i<=7 && i>=5) || (j==17 && i<=7 && i>=5) || (j==7 && i==12) || (j==7 && i==0) || (j==11 && i==0) || (j==11 && i==12)){
+						ajouterCase(Couleur_du_case.GRIS);
+					}else{
 						if((j==9 && i==0) || (j==8 && i==1) || (j==10 && i==1) || (j==7 && i==2) || (j==9 && i==2) || (j==11 && i==2)){
 							ajouterCase(Couleur_du_case.BLANC);
 							getCase(i,j).add(creerPion(Couleur_du_pion.VERT, true));
+						}else{
+							if((j==11 && i==10) || (j==9 && i==10) || (j==7 && i==10) || (j==8 && i==11) || (j==10 && i==11) || (j==9 && i==12)){
+								ajouterCase(Couleur_du_case.BLANC);
+								getCase(i,j).add(creerPion(Couleur_du_pion.ROUGE, true));
+							}else{
+								ajouterCase(Couleur_du_case.BLANC);
+							}
 						}
-						else{
-							
+					}		
+				}else if(nbjoueurs==3){	
+					if ((j%2!=0 && i%2!=0) || (i%2==0 && j%2==0) || (i>=13) || (j<=6 && i<=2) || (j>=12 && i<=2) || (i<=12 && i>=10 && j<=6) || (j>=12 && i<=12 && i>=10) || ( j==0 && i>=4 && i<=8) || ( j==18 && i>=4 && i<=8) || (j==1 && i<=7 && i>=5) || (j==17 && i<=7 && i>=5) || (j==7 && i==12) || (j==7 && i==0) || (j==11 && i==0) || (j==11 && i==12)){
+						ajouterCase(Couleur_du_case.GRIS);
+					}else{
+						if((j==9 && i==0) || (j==8 && i==1) || (j==10 && i==1) || (j==7 && i==2) || (j==9 && i==2) || (j==11 && i==2)){
+							ajouterCase(Couleur_du_case.BLANC);
+							getCase(i,j).add(creerPion(Couleur_du_pion.VERT, true));
+						}else{
+							if((j==2 && i==7) || (j==3 && i==8) || (j==1 && i==8) || (j==0 && i==9) || (j==2 && i==9) || (j==4 && i==9)){
+								ajouterCase(Couleur_du_case.BLANC);
+								getCase(i,j).add(creerPion(Couleur_du_pion.JAUNE, true));
+							}else{
+								if((j==16 && i==7) || (j==15 && i==8) || (j==17 && i==8) || (j==14 && i==9) || (j==16 && i==9) || (j==18 && i==9)){
+									ajouterCase(Couleur_du_case.BLANC);
+									getCase(i,j).add(creerPion(Couleur_du_pion.BLEU, true));
+								}else{
+								ajouterCase(Couleur_du_case.BLANC);
+								}
+							}
+						}
+					}
+				}else if(nbjoueurs==4){	
+					if ((j%2!=0 && i%2!=0) || (i%2==0 && j%2==0) || (i>=13) || (j<=6 && i<=2) || (j>=12 && i<=2) || (i<=12 && i>=10 && j<=6) || (j>=12 && i<=12 && i>=10) || ( j==0 && i>=4 && i<=8) || ( j==18 && i>=4 && i<=8) || (j==1 && i<=7 && i>=5) || (j==17 && i<=7 && i>=5) || (j==7 && i==12) || (j==7 && i==0) || (j==11 && i==0) || (j==11 && i==12)){
+						ajouterCase(Couleur_du_case.GRIS);
+					}else{
+						if((j==9 && i==0) || (j==8 && i==1) || (j==10 && i==1) || (j==7 && i==2) || (j==9 && i==2) || (j==11 && i==2)){
+							ajouterCase(Couleur_du_case.BLANC);
+							getCase(i,j).add(creerPion(Couleur_du_pion.VERT, true));
+						}else{
+							if((j==14 && i==3) || (j==16 && i==3) || (j==18 && i==3) || (j==15 && i==4) || (j==17 && i==4) || (j==16 && i==5)){
+								ajouterCase(Couleur_du_case.BLANC);
+								getCase(i,j).add(creerPion(Couleur_du_pion.ROSE, true));
+							}else{
+								if((j==11 && i==10) || (j==9 && i==10) || (j==7 && i==10) || (j==8 && i==11) || (j==10 && i==11) || (j==9 && i==12)){
+									ajouterCase(Couleur_du_case.BLANC);
+									getCase(i,j).add(creerPion(Couleur_du_pion.ROUGE, true));
+								}else{
+									if((j==2 && i==7) || (j==3 && i==8) || (j==1 && i==8) || (j==0 && i==9) || (j==2 && i==9) || (j==4 && i==9)){
+										ajouterCase(Couleur_du_case.BLANC);
+										getCase(i,j).add(creerPion(Couleur_du_pion.JAUNE, true));
+									}else{
+										ajouterCase(Couleur_du_case.BLANC);
+									}
+								}
+							}
+						}
+					}
+				}else if(nbjoueurs==6){	
+					if ((j%2!=0 && i%2!=0) || (i%2==0 && j%2==0) || (i>=13) || (j<=6 && i<=2) || (j>=12 && i<=2) || (i<=12 && i>=10 && j<=6) || (j>=12 && i<=12 && i>=10) || ( j==0 && i>=4 && i<=8) || ( j==18 && i>=4 && i<=8) || (j==1 && i<=7 && i>=5) || (j==17 && i<=7 && i>=5) || (j==7 && i==12) || (j==7 && i==0) || (j==11 && i==0) || (j==11 && i==12)){
+						ajouterCase(Couleur_du_case.GRIS);
+					}else{
+						if((j==9 && i==0) || (j==8 && i==1) || (j==10 && i==1) || (j==7 && i==2) || (j==9 && i==2) || (j==11 && i==2)){
+							ajouterCase(Couleur_du_case.BLANC);
+							getCase(i,j).add(creerPion(Couleur_du_pion.VERT, true));
+						}else{
 							if((j==0 && i==3) || (j==2 && i==3) || (j==4 && i==3) || (j==1 && i==4) || (j==3 && i==4) || (j==2 && i==5)){
 								ajouterCase(Couleur_du_case.BLANC);
 								getCase(i,j).add(creerPion(Couleur_du_pion.NOIR, true));
-							}
-							else{
-						
+							}else{
 								if((j==14 && i==3) || (j==16 && i==3) || (j==18 && i==3) || (j==15 && i==4) || (j==17 && i==4) || (j==16 && i==5)){
 									ajouterCase(Couleur_du_case.BLANC);
 									getCase(i,j).add(creerPion(Couleur_du_pion.ROSE, true));
-								}
-								else{
-					
+								}else{
 									if((j==16 && i==7) || (j==15 && i==8) || (j==17 && i==8) || (j==14 && i==9) || (j==16 && i==9) || (j==18 && i==9)){
 										ajouterCase(Couleur_du_case.BLANC);
 										getCase(i,j).add(creerPion(Couleur_du_pion.BLEU, true));
-									}
-									else{
-					
+									}else{
 										if((j==11 && i==10) || (j==9 && i==10) || (j==7 && i==10) || (j==8 && i==11) || (j==10 && i==11) || (j==9 && i==12)){
 											ajouterCase(Couleur_du_case.BLANC);
 											getCase(i,j).add(creerPion(Couleur_du_pion.ROUGE, true));
-										}
-										else{
-					
+										}else{
 											if((j==2 && i==7) || (j==3 && i==8) || (j==1 && i==8) || (j==0 && i==9) || (j==2 && i==9) || (j==4 && i==9)){
 												ajouterCase(Couleur_du_case.BLANC);
 												getCase(i,j).add(creerPion(Couleur_du_pion.JAUNE, true));
-												}
-											else{
+											}else{
 												ajouterCase(Couleur_du_case.BLANC);
 											}
 										}
@@ -86,21 +140,12 @@ public class Plateau extends JPanel {
 							}
 						}
 					}
-			
-		
-	
-				
+				}
 			}
 		}
+	}
 	
 		
-		
-		
-		
-	}
-
-
-
 	private void ajouterCase(Couleur_du_case couleur){
 		Cases_du_plateau case_voulue = new Cases_du_plateau(couleur);
 		case_voulue.addMouseListener(new Listener_Cases_du_plateau(case_voulue, this));
@@ -124,7 +169,7 @@ public class Plateau extends JPanel {
 
 
 	public void afficherPossibilites(Pion p){
-		if((p.getCouleur().equals(Couleur_du_pion.VERT) && tourNoir) || (p.getCouleur().equals(Couleur_du_pion.ROUGE) && !tourNoir)){ // si le pion selectionné est noir et que c'est au tour du noir de jouer (ou l'inverse)
+		if((p.getCouleur().equals(Couleur_du_pion.VERT) && tourjoueur.equals(tour.tourVERT)) || (p.getCouleur().equals(Couleur_du_pion.ROUGE) && tourjoueur.equals(tour.tourROUGE))|| (p.getCouleur().equals(Couleur_du_pion.NOIR) && tourjoueur.equals(tour.tourNOIR))|| (p.getCouleur().equals(Couleur_du_pion.BLEU) && tourjoueur.equals(tour.tourBLEU))|| (p.getCouleur().equals(Couleur_du_pion.JAUNE) && tourjoueur.equals(tour.tourJAUNE))|| (p.getCouleur().equals(Couleur_du_pion.ROSE) && tourjoueur.equals(tour.tourROSE))){ // si le pion selectionné est noir et que c'est au tour du noir de jouer (ou l'inverse)
 			int i=0;
 			int j=0;
 			for(int k=0; k<taille*taille; k++){  // le nombre k représente le nombre totale de cases du plateau
@@ -150,21 +195,60 @@ public class Plateau extends JPanel {
 	}
 
 	public void deplacer(Cases_du_plateau case_voulue){
-		case_voulue.add(caseActive.getComponent(0));		//on met le pion de la case active sur la case voulue
-		for(int k=0; k<taille*taille; k++){ 		// reinitialise selectionner_cases
-			getCase(k).setSelectionnee(false);
-		}
-		if(Math.abs(getLigne(case_voulue)-getLigne(caseActive))==2){ // si on doit sauter un pion pour aller dans la case voulue
-			int i = (getLigne(case_voulue)+getLigne(caseActive))/2;	// on obtient le j du pion qui va être sauté
-			int j = (getColonne(case_voulue)+getColonne(caseActive))/2; // on obtient le j du pion qui va être sauté
-			getCase(i, j).removeAll();	// enlève le pion de la case
-			getCase(i, j).validate();	// La méthode validate() permet de demander la validation des données d'un Javabean (je sais pas trop ce que c'est...)
-			getCase(i, j).repaint();	// met à jour le panel
-		}
+		
 		if(finDeJeu() == false) {
-			tourNoir = !tourNoir; // une fois le pion déplacé, c'est au tour de l'autre joueur
-		} else {
-			System.out.println("finDeJeu");
+			case_voulue.add(caseActive.getComponent(0));		//on met le pion de la case active sur la case voulue
+			for(int k=0; k<taille*taille; k++){ 		// reinitialise selectionner_cases
+				getCase(k).setSelectionnee(false);
+			}
+			if(Math.abs(getLigne(case_voulue)-getLigne(caseActive))==2){ // si on doit sauter un pion pour aller dans la case voulue
+				int i = (getLigne(case_voulue)+getLigne(caseActive))/2;	// on obtient le j du pion qui va être sauté
+				int j = (getColonne(case_voulue)+getColonne(caseActive))/2; // on obtient le j du pion qui va être sauté
+				getCase(i, j).removeAll();	// enlève le pion de la case
+				getCase(i, j).validate();	// La méthode validate() permet de demander la validation des données d'un Javabean (je sais pas trop ce que c'est...)
+				getCase(i, j).repaint();	// met à jour le panel
+			}
+			if (nbjoueurs==2) {
+				if (tourjoueur.equals(tour.tourVERT)){
+					tourjoueur=tour.tourROUGE; // une fois le pion déplacé, c'est au tour de l'autre joueur
+				} else {
+					tourjoueur=tour.tourVERT;
+				}
+			}else if (nbjoueurs==3){ 
+				if (tourjoueur.equals(tour.tourVERT)){
+					tourjoueur=tour.tourBLEU; // une fois le pion déplacé, c'est au tour de l'autre joueur
+				} else if (tourjoueur.equals(tour.tourBLEU)) {
+					tourjoueur=tour.tourJAUNE;
+				} else if (tourjoueur.equals(tour.tourJAUNE)) {
+					tourjoueur=tour.tourVERT;
+				}
+			}else if (nbjoueurs==4) {
+				if (tourjoueur.equals(tour.tourVERT)){
+					tourjoueur=tour.tourROSE; // une fois le pion déplacé, c'est au tour de l'autre joueur
+				} else if (tourjoueur.equals(tour.tourROSE)) {
+					tourjoueur=tour.tourROUGE;
+				} else if (tourjoueur.equals(tour.tourROUGE)) {
+					tourjoueur=tour.tourJAUNE;
+				} else if (tourjoueur.equals(tour.tourJAUNE)) {
+					tourjoueur=tour.tourVERT;
+				}
+			} else if (nbjoueurs==6) {
+				if (tourjoueur.equals(tour.tourVERT)){
+					tourjoueur=tour.tourROSE; // une fois le pion déplacé, c'est au tour de l'autre joueur
+				} else if (tourjoueur.equals(tour.tourROSE)) {
+					tourjoueur=tour.tourBLEU;
+				} else if (tourjoueur.equals(tour.tourBLEU)) {
+					tourjoueur=tour.tourROUGE;
+				} else if (tourjoueur.equals(tour.tourROUGE)) {
+					tourjoueur=tour.tourJAUNE;
+				}else if (tourjoueur.equals(tour.tourJAUNE)) {
+					tourjoueur=tour.tourNOIR;
+				} else if (tourjoueur.equals(tour.tourNOIR)) {
+					tourjoueur=tour.tourVERT;
+				}
+			}
+		}else {
+			System.out.println("fin de jeu");
 		}
 		caseActive.removeAll(); // on enlève le pion de sa case précédente
 		caseActive.repaint();	// met à jour le panel 
@@ -236,7 +320,7 @@ public class Plateau extends JPanel {
 		boolean finiBleu = true;
 		boolean finiJaune = true;
 		boolean finiRose = true;
-
+	
 		for (Coordonees c :triangleVert) {
 			if ((getCase(c.getI(),c.getJ()).getComponentCount() == 0)){
 				finiVert = false;
@@ -448,7 +532,6 @@ public class Plateau extends JPanel {
 
 		}
 	}
-
 
 	private int getColonne(Cases_du_plateau case_voulue){
 		int res=0;
