6e6e21c80d8ee46fdc18f0db2fea6c450d2cf809
Ilana <62946166+ilanabidon@users.noreply.github.com>
2020-05-03 13:50:24 +0200

    Update Coordonees.java

--- Coordonees.java
+++ Coordonees.java
@@ -3,26 +3,26 @@ public class Coordonees {
     private int i,j;
     protected boolean dejaSaute;
 
-    public Coordonees(int i, int j){
-        this.i=i;
-        this.j=j;
-        dejaSaute=false;
+    public Coordonees(int i, int j){    // on crée l'objet coordonnées
+        this.i=i;                       // i est la ligne du tableau
+        this.j=j;                       // j est la colonne du tableau
+        dejaSaute=false;                // dejaSaute fait référence au fait qu'un pion ait été sauté par un autre pion ou non
     }
 
-    public int getI(){
+    public int getI(){      // méthode qui donne la valeur de la ligne
         return i;
     }
 
-    public int getJ(){
+    public int getJ(){      // méthode qui donne la valeur de la colonne
         return j;
     }
 
-    public boolean equals(Object o){
+    public boolean equals(Object o){    // méthode qui retourne les mêmes coordonnées que l'objet o 
         Coordonees c = (Coordonees)o;
         return c.getI()==i && c.getJ()==j;
     }
 
-    public String toString(){
+    public String toString(){  // méthode qui retourne la valeur de la ligne et de la colonne
         return "i="+i+" || j="+j;
     }
 }
