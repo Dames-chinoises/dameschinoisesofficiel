f7dd8b072631051e34a3468aee105ffdb37729eb
Ilana <62946166+ilanabidon@users.noreply.github.com>
2020-05-03 14:04:26 +0200

    Update tour.java

--- tour.java
+++ tour.java
@@ -1,8 +1,8 @@
-public enum tour {
+public enum tour {  // cette classe énumère les possibilités de changements de joueurs
 	
 
-	tourNOIR,
-	tourVERT,
+	tourNOIR,  // c'est au tour du joueur qui a les pions noirs
+	tourVERT,  // // c'est au tour du joueur qui a les pions verts et ainsi de suite pour les autres couleurs
 	tourBLEU,
 	tourROUGE,
 	tourJAUNE,
